99b19f7de00bccf65e632a81ce8a818b61bbb6df
Bruno Skvorc <bruno@skvorc.me>
2016-12-29 22:16:01 +0100

    Fixed all the bugs introduced by a new Homestead original version

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -14,7 +14,7 @@ class Homestead
 
     # Configure The Box
     config.vm.box = settings["box"] ||= "laravel/homestead"
-    config.vm.box_version = settings["version"] ||= ">= 0"
+    config.vm.box_version = settings["version"] ||= ">= 1.0.0"
     config.vm.hostname = settings["hostname"] ||= "homestead"
 
     # Configure A Private Network IP
@@ -32,24 +32,30 @@ class Homestead
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", settings["natdnshostresolver"] ||= "on"]
       vb.customize ["modifyvm", :id, "--ostype", "Ubuntu_64"]
+      vb.customize ['modifyvm', :id, '--cableconnected1', 'on']
+      if settings.has_key?("gui") && settings["gui"]
+          vb.gui = true
+      end
     end
 
     # Configure A Few VMware Settings
     ["vmware_fusion", "vmware_workstation"].each do |vmware|
       config.vm.provider vmware do |v|
-        v.vmx["displayName"] = "homestead"
         v.vmx["memsize"] = settings["memory"] ||= 2048
         v.vmx["numvcpus"] = settings["cpus"] ||= 1
         v.vmx["guestOS"] = "ubuntu-64"
+        if settings.has_key?("gui") && settings["gui"]
+            v.gui = true
+        end
       end
     end
 
     # Configure A Few Parallels Settings
     config.vm.provider "parallels" do |v|
+      v.name = settings["name"] ||= "homestead-7"
       v.update_guest_tools = true
-      v.optimize_power_consumption = false
       v.memory = settings["memory"] ||= 2048
       v.cpus = settings["cpus"] ||= 1
     end
@@ -74,9 +80,11 @@ class Homestead
     }
 
     # Use Default Port Forwarding Unless Overridden
-    default_ports.each do |guest, host|
-      unless settings["ports"].any? { |mapping| mapping["guest"] == guest }
-        config.vm.network "forwarded_port", guest: guest, host: host, auto_correct: true
+    unless settings.has_key?("default_ports") && settings["default_ports"] == false
+      default_ports.each do |guest, host|
+        unless settings["ports"].any? { |mapping| mapping["guest"] == guest }
+          config.vm.network "forwarded_port", guest: guest, host: host, auto_correct: true
+        end
       end
     end
 
@@ -91,7 +99,7 @@ class Homestead
     if settings.include? 'authorize'
       if File.exists? File.expand_path(settings["authorize"])
         config.vm.provision "shell" do |s|
-          s.inline = "echo $1 | grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys || echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+          s.inline = "echo $1 | grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys || echo \"\n$1\" | tee -a /home/vagrant/.ssh/authorized_keys"
           s.args = [File.read(File.expand_path(settings["authorize"]))]
         end
       end
@@ -108,13 +116,25 @@ class Homestead
       end
     end
 
+    # Copy User Files Over to VM
+    if settings.include? 'copy'
+      settings["copy"].each do |file|
+        config.vm.provision "file" do |f|
+          f.source = File.expand_path(file["from"])
+          f.destination = file["to"].chomp('/') + "/" + file["from"].split('/').last
+        end
+      end
+    end
+
     # Register All Of The Configured Shared Folders
     if settings.include? 'folders'
       settings["folders"].each do |folder|
         mount_opts = []
 
         if (folder["type"] == "nfs")
-            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1']
+            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1', 'nolock']
+        elsif (folder["type"] == "smb")
+            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['vers=3.02', 'mfsymlinks']
         end
 
         # For b/w compatibility keep separate 'mount_opts', but merge with options
@@ -124,6 +144,11 @@ class Homestead
         options.keys.each{|k| options[k.to_sym] = options.delete(k) }
 
         config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil, **options
+
+        # Bindfs support to fix shared folder (NFS) permission issue on Mac
+        if Vagrant.has_plugin?("vagrant-bindfs")
+          config.bindfs.bind_folder folder["to"], folder["to"]
+        end
       end
     end
 
@@ -133,33 +158,70 @@ class Homestead
     end
 
 
-    settings["sites"].each do |site|
-      config.vm.provision "shell" do |s|
-          if (site.has_key?("hhvm") && site["hhvm"])
-            s.path = scriptDir + "/serve-hhvm.sh"
-            s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
-          elsif (site.has_key?("type") && (site["type"] == "symfony" || site["type"] == "symfony2"))
-            s.path = scriptDir + "/serve-symfony2.sh"
-            s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
-          elsif (site.has_key?("type") && (site["type"] == "symfony-sulu"))
-            s.path = scriptDir + "/serve-sulu.sh"
-            s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
-          else
-            s.path = scriptDir + "/serve.sh"
-            s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
+    if settings.include? 'sites'
+      settings["sites"].each do |site|
+        type = site["type"] ||= "laravel"
+
+        if (site.has_key?("hhvm") && site["hhvm"])
+          type = "hhvm"
+        end
+
+        if (type == "symfony")
+          type = "symfony2"
+        end
+
+        if (type == "symfony-sulu")
+            type = "sulu"
+        end
+
+        config.vm.provision "shell" do |s|
+          s.name = "Creating Site: " + site["map"]
+          s.path = scriptDir + "/serve-#{type}.sh"
+          s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
+        end
+
+        # Configure The Cron Schedule
+        if (site.has_key?("schedule"))
+          config.vm.provision "shell" do |s|
+            s.name = "Creating Schedule"
+
+            if (site["schedule"])
+              s.path = scriptDir + "/cron-schedule.sh"
+              s.args = [site["map"].tr('^A-Za-z0-9', ''), site["to"]]
+            else
+              s.inline = "rm -f /etc/cron.d/$1"
+              s.args = [site["map"].tr('^A-Za-z0-9', '')]
+            end
           end
+        end
+
       end
     end
+
+    config.vm.provision "shell" do |s|
+      s.name = "Restarting Nginx"
+      s.inline = "sudo service nginx restart; sudo service php7.1-fpm restart"
+    end
+
+    # Install MariaDB If Necessary
+    if settings.has_key?("mariadb") && settings["mariadb"]
+      config.vm.provision "shell" do |s|
+        s.path = scriptDir + "/install-maria.sh"
+      end
+    end
+
 
     # Configure All Of The Configured Databases
     if settings.has_key?("databases")
         settings["databases"].each do |db|
           config.vm.provision "shell" do |s|
+            s.name = "Creating MySQL Database: " + db
             s.path = scriptDir + "/create-mysql.sh"
             s.args = [db]
           end
 
           config.vm.provision "shell" do |s|
+            s.name = "Creating Postgres Database: " + db
             s.path = scriptDir + "/create-postgres.sh"
             s.args = [db]
           end
@@ -168,6 +230,7 @@ class Homestead
 
     # Configure All Of The Server Environment Variables
     config.vm.provision "shell" do |s|
+        s.name = "Clear Variables"
         s.path = scriptDir + "/clear-variables.sh"
     end
 
@@ -191,7 +254,9 @@ class Homestead
 
     # Update Composer On Every Provision
     config.vm.provision "shell" do |s|
-      s.inline = "/usr/local/bin/composer self-update"
+      s.name = "Update Composer"
+      s.inline = "sudo /usr/local/bin/composer self-update && sudo chown -R vagrant:vagrant /home/vagrant/.composer/"
+      s.privileged = false
     end
 
     # Configure Blackfire.io
