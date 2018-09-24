5127d15205d74bc5ae432ef635b9e9ef16668aeb
Bruno Škvorc <bruno@skvorc.me>
2018-09-24 09:31:04 +0200

    Update homestead.rb

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -203,7 +203,7 @@ class Homestead
 
     config.vm.provision "shell" do |s|
       s.name = "Restarting Nginx"
-      s.inline = "sudo service nginx restart; sudo service php7.1-fpm restart"
+      s.inline = "sudo service nginx restart; sudo service php7.2-fpm restart"
     end
 
     # Install MariaDB If Necessary
@@ -240,7 +240,7 @@ class Homestead
     if settings.has_key?("variables")
       settings["variables"].each do |var|
         config.vm.provision "shell" do |s|
-          s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php/7.1/fpm/php-fpm.conf"
+          s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php/7.2/fpm/php-fpm.conf"
           s.args = [var["key"], var["value"]]
         end
 
@@ -251,7 +251,7 @@ class Homestead
       end
 
       config.vm.provision "shell" do |s|
-        s.inline = "service php7.1-fpm restart"
+        s.inline = "service php7.2-fpm restart"
       end
     end
 
